291c48a1f444b369cbaac078f2ee6f3fc231b3c7
Adrien Lbt <adrienlambert725@msn.com>
2022-01-16 17:09:16 +0100

    --- Implémentation naïve du app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express');
+
+const PORT = process.env.API_CONTAINER_PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.json({ message: 'Hello world' });
+});
+
