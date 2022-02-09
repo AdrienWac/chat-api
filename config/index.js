0c8bff6916eb8d8430f5a2c8bb5eebe67a5989a3
Adrien Lbt <adrienlambert725@msn.com>
2022-02-09 11:06:03 +0100

    --- Ajustements config pour les tests

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 
-if (['development, test'].includes(env)) {
+if (['development', 'test'].includes(env)) {
     require('dotenv').config({ path: '.env' });
 }
 
@@ -26,11 +26,11 @@ const config = {
     test: {
         CLIENT_URL: process.env.CLIENT_URL,
         PORT: process.env.PORT || 3000,
-        DB_HOST: process.env.MYSQL_HOST,
-        DB_USER: process.env.MYSQL_USER,
-        DB_PASSWORD: process.env.MYSQL_PASSWORD,
-        DB_NAME: process.env.MYSQL_DATABASE,
-        DB_PORT: process.env.MYSQL_PORT,
+        DB_HOST: '127.0.0.1',
+        DB_USER: "root",
+        DB_PASSWORD: "secret",
+        DB_NAME: "test_chat",
+        DB_PORT: 3306,
     }
 }
 
