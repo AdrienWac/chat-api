f75b684be6b14c0626b951c8a7886a26baecf08c
Adrien Lbt <adrienlambert725@msn.com>
2022-03-30 22:23:05 +0200

    --- Fix config sequelize database url for migration

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     "development": {
-        "url": "mysql://" + process.env.MYSQL_USER + ":" + process.env.MYSQL_ROOT_PASSWORD + "@" + process.env.MYSQL_HOST + "/" + process.env.DB_NAME,
+        "url": process.env.CLEARDB_DATABASE_URL,
     },
     "test": {
         "username": "root",
@@ -13,11 +13,6 @@ module.exports = {
     },
     "production": {
         "url": process.env.CLEARDB_DATABASE_URL,
-        // "username": process.env.MYSQL_USER,
-        // "password": process.env.MYSQL_PASSWORD,
-        // "database": process.env.DB_NAME,
-        // "host": process.env.MYSQL_HOST,
-        // "dialect": "mysql"
     }
 };
 
